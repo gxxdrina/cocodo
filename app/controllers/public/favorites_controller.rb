6f2e7bf9a64fea70112c12e6acbd1c232e916ccd
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-06-09 08:42:04 +0000

    いいねの非同期通信化 OK

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,6 +3,10 @@ class Public::FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_end_user.favorites.new(post_id: @post.id)
     favorite.save
+    respond_to do |format|
+      format.js
+    end
+    # redirect_to request.referer
   end
   
   def destroy
