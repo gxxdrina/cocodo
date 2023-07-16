fb6267db6e3e2ac41c4358aea9f5d546af465840
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-07-16 10:41:24 +0000

    Update admin

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,6 +3,6 @@ class Admin::HomesController < ApplicationController
   # 全投稿一覧
   def top
     @end_users = EndUser.all
-    @posts = Post.all.page(params[:page]).per(9)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(9)
   end
 end
